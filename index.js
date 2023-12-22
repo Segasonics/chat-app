df89241e0d8d15038dcffa6f91f9b41d599038ed
Segasonics <114384404+Segasonics@users.noreply.github.com>
2023-12-23 03:12:30 +0530

    another one

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken =process.env.TWILIO_AUTH_TOKEN;
 const username = 'your_username'; // Add this line if required
-const twilioClient  = require('twilio')(accountSid, authToken, { username });
+const twilioClient  = require('twilio')(accountSid, authToken, { username:username });
 
 
 app.use(cors());
