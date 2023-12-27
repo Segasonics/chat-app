e3500e5d5dfb6a2eb7f8421eab9ffa92c01e6030
Segasonics <114384404+Segasonics@users.noreply.github.com>
2023-12-28 00:41:54 +0530

    another again

--- index.js
+++ index.js
@@ -12,8 +12,12 @@ require('dotenv').config();
 // const authToken = process.env.TWILIO_AUTH_TOKEN;
 // const messagingServiceSid = process.env.TWILIO_MESSAGING_SERVICE_SID;
 //const twilioClient = require('twilio')(accountSid, authToken);
-
-app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
 app.use(express.json());
 app.use(express.urlencoded());
 
